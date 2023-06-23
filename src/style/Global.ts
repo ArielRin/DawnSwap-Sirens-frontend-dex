6cbade4f834850bc141e1e01956a7c91bd61eed1
ArielRin <girlsmakingcrypto@gmail.com>
2023-06-23 18:08:34 +1000

    Update Global.ts

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -6,7 +6,7 @@ const GlobalStyle = createGlobalStyle`
   }
   body {
 
-  background-image: url('/images/oldbkg.gif');
+  background-image: url('/images/body.png');
   background-size: cover;
   min-height: 100vh; /* required for full page background */
   justifyContent: 'center';
